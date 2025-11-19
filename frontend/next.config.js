202a83b0ae47ab47191ef2124eb82ea63c7f901b
Ayon-exe <ayon02mondal@gmail.com>
2025-11-19 21:38:50 +0530

    more changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -11,4 +11,20 @@ module.exports = {
     env: {
       SERVER_URL: process.env.SERVER_URL,
     }
+  ,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in the application
+        source: '/(.*)',
+        headers: [
+          { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+          { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()' }
+        ],
+      },
+    ];
+  },
   }
